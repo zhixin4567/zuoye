ca6371a5ccd1fd37f11d6c7de33287cccdfa9468
zhixin4567 <1965356036@qq.com>
2016-09-20 10:38:29 +0800

    0920-2

--- 0920.js
+++ 0920.js
@@ -1,3 +1,11 @@
 (function(){
-	
+	for(var i = 0 ;i<array.length;i++){
+		array[i] = i*10;
+
+	}
+	console.log('array ='+array);
+	console.log(''+new Date());
+	// window.setTimeOut(function()){
+	// 	console.log('timeout text');
+	// },2000)
 		 }(window))
