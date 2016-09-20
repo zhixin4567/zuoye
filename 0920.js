f4e9aeb52326b2cad036e871ad4d6ff95af68acc
zhixin4567 <1965356036@qq.com>
2016-09-20 16:14:19 +0800

    0920-3

--- 0920.js
+++ 0920.js
@@ -1,11 +1,28 @@
-(function(){
-	for(var i = 0 ;i<array.length;i++){
-		array[i] = i*10;
+(function() {
+    // for(var i = 0 ;i<array.length;i++){
+    // 	array[i] = i*10;
 
-	}
-	console.log('array ='+array);
-	console.log(''+new Date());
-	// window.setTimeOut(function()){
-	// 	console.log('timeout text');
-	// },2000)
-		 }(window))
+    // }
+    // console.log('array ='+array);
+    // console.log(''+new Date());
+    // window.setTimeOut(function()){
+    // 	console.log('timeout text');
+    // },2000)
+    // function sayHello (name , age){
+    // 	console.log('name:' + name);
+    // 	console.log("age:" + age);
+    // }
+    // window.setTimeout(sayHello,2000,'Ada',19);
+
+    var btn = document.getElementById("a");
+
+
+	btn.onclick = function myFunction() {
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("Water");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+}
+
+}(window))
+
