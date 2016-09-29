f98b9cc80219da5abf129e6d54d9f42bf9a3e17c
zhixin4567 <1965356036@qq.com>
2016-09-29 16:06:10 +0800

    ceshi

--- 0920.js
+++ 0920.js
@@ -1,18 +1,18 @@
 (function() {
-    // for(var i = 0 ;i<array.length;i++){
-    // 	array[i] = i*10;
+    for(var i = 0 ;i<array.length;i++){
+    	array[i] = i*10;
 
-    // }
-    // console.log('array ='+array);
-    // console.log(''+new Date());
-    // window.setTimeOut(function()){
-    // 	console.log('timeout text');
-    // },2000)
-    // function sayHello (name , age){
-    // 	console.log('name:' + name);
-    // 	console.log("age:" + age);
-    // }
-    // window.setTimeout(sayHello,2000,'Ada',19);
+    }
+    console.log('array ='+array);
+    console.log(''+new Date());
+    window.setTimeOut(function()){
+    	console.log('timeout text');
+    },2000)
+    function sayHello (name , age){
+    	console.log('name:' + name);
+    	console.log("age:" + age);
+    }
+    window.setTimeout(sayHello,2000,'Ada',19);
 
     var btn = document.getElementById("a");
 
@@ -23,6 +23,7 @@
     node.appendChild(textnode);
     document.getElementById("myList").appendChild(node);
 }
-
+//shidhid
+function(){}
 }(window))
 
